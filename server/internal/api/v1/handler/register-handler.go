6f57d75d4fae1bdd49fdf537d7545f3f277521e1
raj.kumar.vishal <kumars701113@gmail.com>
2025-07-11 10:42:23 +0530

    feat(server): get profile service handler

--- server/internal/api/v1/handler/register-handler.go
+++ server/internal/api/v1/handler/register-handler.go
@@ -2,11 +2,15 @@ package handler
 
 import (
 	"log"
+	"net/http"
 
 	"connectrpc.com/connect"
 	"connectrpc.com/validate"
 	"github.com/vishalx07/next-mlm/gen/auth/v1/authv1connect"
+	"github.com/vishalx07/next-mlm/gen/user/profile/v1/profilev1connect"
 	root_handler "github.com/vishalx07/next-mlm/internal/api/v1/handler/root"
+	user_handler "github.com/vishalx07/next-mlm/internal/api/v1/handler/user"
+	middleware "github.com/vishalx07/next-mlm/internal/middleware"
 	server "github.com/vishalx07/next-mlm/internal/pkg/server"
 	repo "github.com/vishalx07/next-mlm/internal/repository"
 	service "github.com/vishalx07/next-mlm/internal/service"
@@ -43,9 +47,13 @@ func RegisterHandler(s *server.Server) {
 	mux.Handle(authServicePath, authServiceHandler)
 
 	// =============================== USER SERVICES ===============================
-	// userMw := func(next http.Handler) http.Handler {
-	// 	return middleware.AuthMiddleware(next, env, userService)
-	// }
+	userMw := func(next http.Handler) http.Handler {
+		return middleware.AuthMiddleware(next, env, userService)
+	}
+
+	profileHandler := user_handler.NewProfileHandler(userService)
+	profileServicePath, profileServiceHandler := profilev1connect.NewProfileServiceHandler(profileHandler, connect.WithInterceptors(validateInterceptor))
+	mux.Handle(profileServicePath, userMw(profileServiceHandler))
 
 	// =============================== ADMIN SERVICES ===============================
 }
---
+++ server/internal/api/v1/handler/user/profile.go
@@ -0,0 +1,59 @@
+package user_handler
+
+import (
+	"context"
+
+	"connectrpc.com/connect"
+	typesv1 "github.com/vishalx07/next-mlm/gen/types/v1"
+	profilev1 "github.com/vishalx07/next-mlm/gen/user/profile/v1"
+	middleware "github.com/vishalx07/next-mlm/internal/middleware"
+	models "github.com/vishalx07/next-mlm/internal/models"
+	enums "github.com/vishalx07/next-mlm/internal/pkg/types/enums"
+	service "github.com/vishalx07/next-mlm/internal/service"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type ProfileHandler struct {
+	userService service.UserServiceInterface
+}
+
+func NewProfileHandler(userService service.UserServiceInterface) *ProfileHandler {
+	return &ProfileHandler{userService}
+}
+
+func (h *ProfileHandler) GetProfile(
+	ctx context.Context,
+	req *connect.Request[profilev1.GetProfileRequest],
+) (*connect.Response[profilev1.GetProfileResponse], error) {
+	user := middleware.UserFromContext(ctx)
+
+	resp := connect.NewResponse(&profilev1.GetProfileResponse{
+		User: h.transformUserModel(user),
+	})
+
+	return resp, nil
+}
+
+func (h *ProfileHandler) transformUserModel(user *models.User) *typesv1.User {
+	var avatar string
+	if user.Avatar != nil {
+		avatar = *user.Avatar
+	}
+
+	return &typesv1.User{
+		Id:          user.Id,
+		UserId:      user.UserId,
+		Fullname:    user.Fullname,
+		Username:    user.Username,
+		Email:       user.Email,
+		Role:        enums.UserRoleToProto(user.Role),
+		Status:      enums.UserStatusToProto(user.Status),
+		Avatar:      avatar,
+		Country:     user.Country,
+		PhoneNumber: user.Phone,
+		ReferralId:  user.ReferralId,
+		Level:       user.Level,
+		CreatedAt:   timestamppb.New(user.CreatedAt),
+		UpdatedAt:   timestamppb.New(user.UpdatedAt),
+	}
+}
