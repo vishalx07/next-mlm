3bc23aab835202cd3eec8a002186351a0fc6c9bc
raj.kumar.vishal <kumars701113@gmail.com>
2025-07-14 09:20:15 +0530

    feat(client): add icons

--- client/packages/ui/src/config/icons.ts
+++ client/packages/ui/src/config/icons.ts
@@ -1,5 +1,11 @@
-import ArrowIosBack from "../../public/icons/eva-arrow-ios-back-fill.svg";
-import ArrowIosForward from "../../public/icons/eva-arrow-ios-forward-fill.svg";
+import ArrowIosBack from "../../public/icons/i-eva-arrow-ios-back-fill.svg";
+import ArrowIosForward from "../../public/icons/i-eva-arrow-ios-forward-fill.svg";
+import Mail from "../../public/icons/i-fluent-mail-24-filled.svg";
+import Google from "../../public/icons/i-logos-google-icon.svg";
+import Linkedin from "../../public/icons/i-logos-linkedin-icon.svg";
+import LockPassword from "../../public/icons/i-solar-lock-password-bold.svg";
+import Pen from "../../public/icons/i-solar-pen-bold.svg";
+//
 import Dashboard from "../../public/svg/dashboard/dashboard.svg";
 import Level from "../../public/svg/dashboard/level.svg";
 import Plan from "../../public/svg/dashboard/plan.svg";
@@ -15,4 +21,18 @@ export const DashboardIcon = {
 export const EvaIcon = {
   ArrowIosBack,
   ArrowIosForward,
+};
+
+export const SolarIcon = {
+  LockPassword,
+  Pen,
+};
+
+export const FluentIcon = {
+  Mail,
+};
+
+export const LogosIcon = {
+  Google,
+  Linkedin,
 };
